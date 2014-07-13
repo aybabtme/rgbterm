013e914e5f97fceff2124a2ddeaafd935c21e2dc
Antoine Grondin <antoinegrondin@gmail.com>
2014-07-13 13:50:12 -0400

    Supporting background colors could be a good idea.

--- rgbterm.go
+++ rgbterm.go
@@ -24,8 +24,9 @@
 package rgbterm
 
 var (
-	reset  = []byte("\033[0;00m")
-	colors = termRGB[16:232]
+	reset    = []byte("\033[0;00m")
+	fgcolors = fgTermRGB[16:232]
+	bgcolors = bgTermRGB[16:232]
 )
 
 // String colorizes the input with the terminal color that matches
@@ -36,22 +37,42 @@ func String(in string, r, b, g uint8) string {
 	return string(Bytes([]byte(in), r, g, b))
 }
 
+// BgString colorizes the background of the input with the terminal color
+// that matches the closest the RGB color.
+//
+// This is simply a helper for Bytes.
+func BgString(in string, r, b, g uint8) string {
+	return string(BgBytes([]byte(in), r, g, b))
+}
+
 // Bytes colorizes the input with the terminal color that matches
 // the closest the RGB color.
 func Bytes(in []byte, r, g, b uint8) []byte {
-	return append(append(rgb(r, g, b), in...), reset...)
+	return append(append(rgb(r, g, b, true), in...), reset...)
+}
+
+// BgBytes colorizes the background of the input with the terminal color
+// that matches the closest the RGB color.
+func BgBytes(in []byte, r, g, b uint8) []byte {
+	return append(append(rgb(r, g, b, false), in...), reset...)
 }
 
 // Byte colorizes the input with the terminal color that matches
 // the closest the RGB color.
 func Byte(in byte, r, g, b uint8) []byte {
-	return append(append(rgb(r, g, b), in), reset...)
+	return append(append(rgb(r, g, b, true), in), reset...)
 }
 
-func rgb(r, g, b uint8) []byte {
+// BgByte colorizes the background of the input with the terminal color
+// that matches the closest the RGB color.
+func BgByte(in byte, r, g, b uint8) []byte {
+	return append(append(rgb(r, g, b, false), in), reset...)
+}
+
+func rgb(r, g, b uint8, foreground bool) []byte {
 	// if all colors are equal, it might be in the grayscale range
 	if r == g && g == b {
-		color, ok := grayscale(r)
+		color, ok := grayscale(r, foreground)
 		if ok {
 			return color
 		}
@@ -62,59 +83,71 @@ func rgb(r, g, b uint8) []byte {
 	g6 := ((uint16(g) * 5) / 255)
 	b6 := ((uint16(b) * 5) / 255)
 	i := 36*r6 + 6*g6 + b6
-	return colors[i]
+	if foreground {
+		return fgcolors[i]
+	} else {
+		return bgcolors[i]
+	}
 }
 
-func grayscale(scale uint8) ([]byte, bool) {
+func grayscale(scale uint8, foreground bool) ([]byte, bool) {
+	var source [256][]byte
+
+	if foreground {
+		source = fgTermRGB
+	} else {
+		source = bgTermRGB
+	}
+
 	switch scale {
 	case 0x08:
-		return termRGB[232], true
+		return source[232], true
 	case 0x12:
-		return termRGB[233], true
+		return source[233], true
 	case 0x1c:
-		return termRGB[234], true
+		return source[234], true
 	case 0x26:
-		return termRGB[235], true
+		return source[235], true
 	case 0x30:
-		return termRGB[236], true
+		return source[236], true
 	case 0x3a:
-		return termRGB[237], true
+		return source[237], true
 	case 0x44:
-		return termRGB[238], true
+		return source[238], true
 	case 0x4e:
-		return termRGB[239], true
+		return source[239], true
 	case 0x58:
-		return termRGB[240], true
+		return source[240], true
 	case 0x62:
-		return termRGB[241], true
+		return source[241], true
 	case 0x6c:
-		return termRGB[242], true
+		return source[242], true
 	case 0x76:
-		return termRGB[243], true
+		return source[243], true
 	case 0x80:
-		return termRGB[244], true
+		return source[244], true
 	case 0x8a:
-		return termRGB[245], true
+		return source[245], true
 	case 0x94:
-		return termRGB[246], true
+		return source[246], true
 	case 0x9e:
-		return termRGB[247], true
+		return source[247], true
 	case 0xa8:
-		return termRGB[248], true
+		return source[248], true
 	case 0xb2:
-		return termRGB[249], true
+		return source[249], true
 	case 0xbc:
-		return termRGB[250], true
+		return source[250], true
 	case 0xc6:
-		return termRGB[251], true
+		return source[251], true
 	case 0xd0:
-		return termRGB[252], true
+		return source[252], true
 	case 0xda:
-		return termRGB[253], true
+		return source[253], true
 	case 0xe4:
-		return termRGB[254], true
+		return source[254], true
 	case 0xee:
-		return termRGB[255], true
+		return source[255], true
 	}
 	return nil, false
 }
